325f75b58eae7246bfb3098dac3031fc40916322
Jonathan Kabongo <jnthnkabongo@gmail.com>
2026-06-18 19:37:24 +0100

    le navigationbar

---
+++ lib/pages/navigation_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class HomeWithBottomNav extends StatefulWidget {
+  const HomeWithBottomNav({super.key});
+
+  @override
+  State<HomeWithBottomNav> createState() => _HomeWithBottomNavState();
+}
+
+class _HomeWithBottomNavState extends State<HomeWithBottomNav> {
+  int _index = 0;
+
+  final _pages = const [
+    Center(child: Text("Accueil")),
+    Center(child: Text("Produits")),
+    Center(child: Text("Panier")),
+    Center(child: Text("Profil")),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_index],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _index,
+        onTap: (i) => setState(() => _index = i),
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Accueil"),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_bag), label: "Produits"),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: "Panier"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profil"),
+        ],
+      ),
+    );
+  }
+}
